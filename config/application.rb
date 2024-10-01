5d06760f9c81fedef7766235d878591ab1bfa3b3
ゆうせい <Ikeura0604@gmail.com>
2024-10-01 10:21:32 +0900

    文字の国際化の導入

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,17 @@ module Myapp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # 不要なファイルを生成しない設定
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.skip_routes true      # ルーティングファイルを自動生成しない
+      g.helper false          # ヘルパーファイルを自動生成しない
+      g.test_framework nil    # テストファイルを自動生成しない
+    end
+
+    # デフォルトの言語を日本語に設定
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
