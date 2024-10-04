f5d4b79b5462826aaef35b5a41a19eaffa99cd3c
ゆうせい <Ikeura0604@gmail.com>
2024-10-04 16:41:24 +0900

    投稿の一覧画面の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_20_040751) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_03_014805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_20_040751) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_20_040751) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
