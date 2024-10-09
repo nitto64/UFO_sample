fbe12544f76cc40bbe4b74fb094be3881ad0d2c9
ゆうせい <Ikeura0604@gmail.com>
2024-10-09 10:35:53 +0900

    投稿の詳細ページの作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_one_attached :thumbnail
+  has_one_attached :thumbnail, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
