b105fc9dd124a825e9b6e0a5ec4945ac24d029e1
ゆうせい <Ikeura0604@gmail.com>
2024-10-16 12:32:16 +0900

    複数画像のアップロード機能の追加とスライダーを利用した表示方法の導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :thumbnail, dependent: :destroy
+  has_many_attached :main_images, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
+  validates :main_images, attachment: { content_type: %r{\Aimage/(png|jpeg)\Z}, maximum: 15.megabytes, purge: true }
+  validates :thumbnail, attachment: { content_type: %r{\Aimage/(png|jpeg)\Z}, maximum: 15.megabytes, purge: true }
 end
